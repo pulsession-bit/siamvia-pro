56412f426a3bbee592ba87d5dcc472351b7b4e3e
pulsession-bit <pulsession@gmail.com>
2026-02-16 16:07:59 +0100

    Remove /_next/ from robots.txt disallow list to improve SEO crawling of assets

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: [
                 '/api/',
-                '/_next/',
                 '/private/',
                 '*/test-auth',
                 '*/test-search-ai',
